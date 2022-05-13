d21428a6ec2613b198ffaa97ae1603720cd667b5
Mads Frost <madsanton31@gmail.com>
2022-05-13 14:59:16 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# EduNotes
+WIP
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <div style={{ display: "flex", backgroundColor: 'tomato', width: '100%', height: '100%', margin: 0 }}>
+            App
+        </div>
+    )
+}
+
+export default App;
